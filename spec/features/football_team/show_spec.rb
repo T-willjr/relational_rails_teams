68a4efb0e1533b2b123da731dba4deef38af7064
Paul Leonard <paulpaulleonard@gmail.com>
2022-02-02 14:39:47 -0500

    Fix edge case to be a football team

--- spec/features/football_team/show_spec.rb
+++ spec/features/football_team/show_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 RSpec.describe "Football Team Show" do
   it "when I visit a football team's show page, I see that football team's attributes listed" do
     team1 = FootballTeam.create!(name: "Georgia Bulldogs", public: true, titles: 2)
-    team = BasketballTeam.create!(name: "Atlanta Hawks", winning_record: true, titles: 1)
+    team2 = BasketballTeam.create!(name: "Atlanta Dream", winning_record: true, titles: 0)
     visit "/football_teams/#{team1.id}"
 
     expect(page).to have_content(team1.name)
     expect(page).to have_content(team1.public)
     expect(page).to have_content(team1.titles)
-    expect(page).to_not have_content(team.name)
+    expect(page).to_not have_content(team2.name)
   end
 end
