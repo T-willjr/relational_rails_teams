757614064f141431fbfe9673c575d1e353190b56
TJ Williams <williamsjr.timothy1@gmail.com>
2022-02-02 08:41:12 -0500

    Feat: Displays a Basketball Teams information

---
+++ spec/features/basketball_teams/show_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe "BasketballTeam Information" do
+  it "displays a basetball team's information" do
+    team = BasketballTeam.create!(name: "Atlanta Hawks", winning_record: true, titles: 1)
+    team2 = BasketballTeam.create!(name: "Atlanta Dream", winning_record: true, titles: 0)
+    team3 = BasketballTeam.create!(name: "Georgia Southern Eagles", winning_record: true, titles: 0)
+    visit "/basketballteams/#{team.id}"
+    expect(page).to have_content(team.name)
+    expect(page).to have_content(team.winning_record)
+    expect(page).to have_content(team.titles)
+  end
+end
