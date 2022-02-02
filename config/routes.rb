f88bb03d272d923891152e831c7511fb68f3e052
Paul Leonard <paulpaulleonard@gmail.com>
2022-02-02 12:15:00 -0500

    fix test to have correct class name and create route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/basketballteams', to: 'basketball_teams#index'
+
+
+  get '/football_teams', to: 'football_teams#index'
+
+
+  #we probably want to put spaces between each subject like bball and fball teams
 end
--- spec/features/football_team/index_spec.rb
+++ spec/features/football_team/index_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "Football Teams Index" do
   it "When I visit the football team index, I see the name of every team" do
-    # team1 = FootBallTeam.create!(name: "Georgia Bulldogs", public: true, titles: 2)
-    team2 = FootBallTeam.create!(name: "Georgia Tech", public: true, titles: 1)
-    team3 = FootBallTeam.create!(name: "Georgia Southern", public: true, titles: 5)
+    team1 = FootballTeam.create!(name: "Georgia Bulldogs", public: true, titles: 2)
+    team2 = FootballTeam.create!(name: "Georgia Tech", public: true, titles: 1)
+    team3 = FootballTeam.create!(name: "Georgia Southern", public: true, titles: 5)
     visit "/football_teams"
 
     expect(page).to have_content(team1.name)
