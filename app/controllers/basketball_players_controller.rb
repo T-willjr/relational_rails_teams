9271ed3a3537d27e8d5553cffe54959ca93dd4ec
TJ Williams <williamsjr.timothy1@gmail.com>
2022-02-03 07:48:26 -0500

    Add: Routes,Controller, View for basketball_players#show

--- app/controllers/basketball_players_controller.rb
+++ app/controllers/basketball_players_controller.rb
@@ -2,4 +2,8 @@ class BasketballPlayersController < ApplicationController
   def index
     @players = BasketballPlayer.all
   end
+
+  def show
+    @player = BasketballPlayer.find(params[:id])
+  end
 end
