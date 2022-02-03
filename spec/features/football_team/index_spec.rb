3bc09047d434593fe67b02020919a603d9b27276
Paul Leonard <paulpaulleonard@gmail.com>
2022-02-03 17:22:23 -0500

    write new test in football team #index spec for ordering by most recently

--- spec/features/football_team/index_spec.rb
+++ spec/features/football_team/index_spec.rb
@@ -14,4 +14,16 @@ RSpec.describe "Football Teams Index" do
     expect(page).to have_content(team3.name)
     expect(page).to_not have_content(team.name)
   end
+
+  it "has teams listed by most recently created" do
+    team1 = FootballTeam.create!(name: "Georgia Bulldogs", public: false, titles: 2)
+    team2 = FootballTeam.create!(name: "Georgia Tech", public: true, titles: 1)
+    team3 = FootballTeam.create!(name: "Georgia Southern", public: true, titles: 5)
+
+    visit "/football_teams"
+
+    expect(team3.name).to appear_before(team2.name)
+    expect(team2.name).to appear_before(team1.name)
+    expect(team1.name).to_not appear_before(team3.name)
+  end
 end
