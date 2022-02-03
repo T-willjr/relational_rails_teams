ccc42492f86386f00fcfa6a63d5aba5ef3bdb2d1
TJ Williams <williamsjr.timothy1@gmail.com>
2022-02-03 09:07:08 -0500

    Test: Orders Basketball Team object by created_at date

--- spec/models/basketball_team_spec.rb
+++ spec/models/basketball_team_spec.rb
@@ -12,4 +12,13 @@ RSpec.describe BasketballTeam do
     it {should allow_value(nil).for(:winning_record) }
     it {should validate_presence_of :titles }
   end
+
+  describe "#order_by_most_recently_created" do
+    it "should order basketball teams by most recently created" do
+      team = BasketballTeam.create!(name: "Atlanta Hawks", winning_record: true, titles: 1)
+      team2 = BasketballTeam.create!(name: "Atlanta Dream", winning_record: true, titles: 0)
+      team3 = BasketballTeam.create!(name: "Georgia Southern Eagles", winning_record: true, titles: 0)
+      expect(BasketballTeam.order_by_most_recently_created).to eq([team,team2,team3])
+    end
+  end
 end
