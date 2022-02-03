92e797624a0d3af06229505aab73285a1e7b2811
TJ Williams <williamsjr.timothy1@gmail.com>
2022-02-03 09:08:26 -0500

    Test: Displays dates when a Basketball Teams object was created

--- spec/features/basketball_teams/index_spec.rb
+++ spec/features/basketball_teams/index_spec.rb
@@ -12,4 +12,16 @@ RSpec.describe "BasketballTeams" do
     expect(page).to have_content(team2.name)
     expect(page).to have_content(team3.name)
   end
+
+  it "displays dates when Basketball Teams record was created" do
+    team = BasketballTeam.create!(name: "Atlanta Hawks", winning_record: true, titles: 1)
+    team2 = BasketballTeam.create!(name: "Atlanta Dream", winning_record: true, titles: 0)
+    team3 = BasketballTeam.create!(name: "Georgia Southern Eagles", winning_record: true, titles: 0)
+
+    visit '/basketball_teams'
+
+    expect(page).to have_content(team.created_at)
+    expect(page).to have_content(team2.created_at)
+    expect(page).to have_content(team3.created_at)
+  end
 end
