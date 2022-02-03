036c5b454d67ce726b245431b33ad65c82c3881a
TJ Williams <williamsjr.timothy1@gmail.com>
2022-02-03 14:18:18 -0500

    Test: Returns number of players on a team

--- spec/models/basketball_team_spec.rb
+++ spec/models/basketball_team_spec.rb
@@ -21,4 +21,21 @@ RSpec.describe BasketballTeam do
       expect(BasketballTeam.order_by_most_recently_created).to eq([team,team2,team3])
     end
   end
+
+  describe "#number_of_players" do
+    it "should count the numbers of players on a team" do
+      team = BasketballTeam.create!(name: "Atlanta Hawks", winning_record: true, titles: 1)
+      team2 = BasketballTeam.create!(name: "Atlanta Dream", winning_record: true, titles: 0)
+      team3 = BasketballTeam.create!(name: "Georgia Southern Eagles", winning_record: true, titles: 0)
+      team4 = BasketballTeam.create!(name: "Georgia Tech", winning_record: false, titles: 0)
+      player = team.basketball_players.create!(name: "Trae Young", injured: true, jersey_number: 11)
+      player2 = team.basketball_players.create!(name: "John Collins", injured: false, jersey_number: 20)
+      player3 = team2.basketball_players.create!(name: "Odyssey Sims", injured: false, jersey_number: 0)
+      player4 = team3.basketball_players.create!(name: "Jordan Usher", injured: false, jersey_number: 4)
+      expect(team.number_of_players).to eq(2)
+      expect(team2.number_of_players).to eq(1)
+      expect(team3.number_of_players).to eq(1)
+      expect(team4.number_of_players).to eq(0)
+    end
+  end
 end
