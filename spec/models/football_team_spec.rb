531a9ac4599cc1c82627c7a199872a18b628571e
Paul Leonard <paulpaulleonard@gmail.com>
2022-02-03 17:19:09 -0500

    write test for class method order_by_most_recently_created

--- spec/models/football_team_spec.rb
+++ spec/models/football_team_spec.rb
@@ -11,4 +11,15 @@ RSpec.describe FootballTeam do
     it {should allow_value(false).for(:public) }
     it {should validate_presence_of :titles }
   end
+
+  describe 'class methods' do
+    it "can order all instances of class by most recently created" do
+      team = FootballTeam.create!(name: "Georgia Bulldogs", public: false, titles: 2)
+      team2 = FootballTeam.create!(name: "Georgia Tech", public: true, titles: 2)
+
+      expect(FootballTeam.first).to eq(team)
+      expect(FootballTeam.last).to eq(team2)
+      expect(FootballTeam.order_by_most_recently_created).to eq([team2, team])
+    end
+  end
 end
