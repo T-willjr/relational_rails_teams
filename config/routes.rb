86bd4a264cb302d77bee868611f9906f8e10de23
Paul Leonard <paulpaulleonard@gmail.com>
2022-02-05 15:49:41 -0500

    Add route to FBT #new

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
 
   get '/football_teams', to: 'football_teams#index'
+  get '/football_teams/new', to: 'football_teams#new'
   get '/football_teams/:id', to: 'football_teams#show'
 
   get '/football_players', to: 'football_players#index'
