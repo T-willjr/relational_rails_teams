2d710cc891f38778cb59b2cae60c080b85e54d14
TJ Williams <williamsjr.timothy1@gmail.com>
2022-02-07 17:49:05 -0500

    Add: Create & params methods to basketball_teams_player controller

--- app/controllers/basketball_team_players_controller.rb
+++ app/controllers/basketball_team_players_controller.rb
@@ -5,5 +5,18 @@ class BasketballTeamPlayersController < ApplicationController
   end
 
   def new
-  end 
+    @team = BasketballTeam.find(params[:id])
+  end
+
+  def create
+    team = BasketballTeam.find(params[:id])
+    @player = team.basketball_players.create(player_params)
+    redirect_to "/basketball_teams/#{team.id}/players"
+  end
+
+  private
+
+  def player_params
+    params.permit(:name, :injured, :jersey_number)
+  end
 end
