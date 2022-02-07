277cf131e6fc122f5e27418900d21b50c813fe06
TJ Williams <williamsjr.timothy1@gmail.com>
2022-02-07 18:43:11 -0500

    Add: Edit, update, params methods

--- app/controllers/basketball_players_controller.rb
+++ app/controllers/basketball_players_controller.rb
@@ -6,4 +6,20 @@ class BasketballPlayersController < ApplicationController
   def show
     @player = BasketballPlayer.find(params[:id])
   end
+
+  def edit
+    @player = BasketballPlayer.find(params[:id])
+  end
+
+  def update
+    @player = BasketballPlayer.find(params[:id])
+    @player.update(player_params)
+    redirect_to "/basketball_players/#{@player.id}"
+  end
+
+  private
+
+  def player_params
+    params.permit(:name, :injured, :jersey_number)
+  end
 end
