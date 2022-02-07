8ea0b56bc6f1c17d3edeb9bb5a2da276eb57dd93
TJ Williams <williamsjr.timothy1@gmail.com>
2022-02-07 16:01:38 -0500

    Add: Edit and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/basketball_teams', to: 'basketball_teams#index'
   get '/basketball_teams/new', to: 'basketball_teams#new'
-  get '/basketball_teams/:id/edit', to: 'basketball_teams#edit'
   get '/basketball_teams/:id', to: 'basketball_teams#show'
+  get '/basketball_teams/:id/edit', to: 'basketball_teams#edit'
   post '/basketball_teams', to: 'basketball_teams#create'
+  patch '/basketball_teams/:id', to: 'basketball_teams#update'
 
   get '/basketball_players', to: 'basketball_players#index'
   get '/basketball_players/:id', to: 'basketball_players#show'
