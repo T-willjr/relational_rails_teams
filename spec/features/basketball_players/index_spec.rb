d08849eea9cc324df3dfc3a7892ec831ff6520d7
TJ Williams <williamsjr.timothy1@gmail.com>
2022-02-07 20:46:07 -0500

    Test: Displays only #injured players

--- spec/features/basketball_players/index_spec.rb
+++ spec/features/basketball_players/index_spec.rb
@@ -6,8 +6,8 @@ RSpec.describe "Basketball Players" do
     team2 = BasketballTeam.create!(name: "Atlanta Dream", winning_record: true, titles: 0)
     team3 = BasketballTeam.create!(name: "Georgia Southern Eagles", winning_record: true, titles: 0)
     player = team.basketball_players.create!(name: "Trae Young", injured: true, jersey_number: 11)
-    player2 = team2.basketball_players.create!(name: "Odyssey Sims", injured: false, jersey_number: 0)
-    player3 = team3.basketball_players.create!(name: "Jordan Usher", injured: false, jersey_number: 4)
+    player2 = team2.basketball_players.create!(name: "Odyssey Sims", injured: true, jersey_number: 0)
+    player3 = team3.basketball_players.create!(name: "Jordan Usher", injured: true, jersey_number: 4)
     visit '/basketball_players'
     expect(page).to have_content(player.name)
     expect(page).to have_content(player.injured)
@@ -24,5 +24,26 @@ RSpec.describe "Basketball Players" do
     visit '/basketball_players'
     click_link "Basketball Players"
     expect(current_path).to eq("/basketball_players")
+  end
+
+  it "displays only #injured? players" do
+    team = BasketballTeam.create!(name: "Atlanta Hawks", winning_record: true, titles: 1)
+    team2 = BasketballTeam.create!(name: "Atlanta Dream", winning_record: true, titles: 0)
+    team3 = BasketballTeam.create!(name: "Georgia Southern Eagles", winning_record: true, titles: 0)
+    player = team.basketball_players.create!(name: "Trae Young", injured: true, jersey_number: 11)
+    player2 = team.basketball_players.create!(name: "John Collins", injured: true, jersey_number: 20)
+    player3 = team2.basketball_players.create!(name: "Odyssey Sims", injured: false, jersey_number: 0)
+    player4 = team3.basketball_players.create!(name: "Jordan Usher", injured: false, jersey_number: 4)
+    visit '/basketball_players'
+    expect(page).to have_content(player.name)
+    expect(page).to have_content(player.injured)
+    expect(page).to have_content(player.jersey_number)
+    expect(page).to have_content(player2.name)
+    expect(page).to have_content(player2.injured)
+    expect(page).to have_content(player2.jersey_number)
+    expect(page).to_not have_content(player3.name)
+    expect(page).to_not have_content(player3.injured)
+    expect(page).to_not have_content(player4.name)
+    expect(page).to_not have_content(player4.injured)
   end
 end
