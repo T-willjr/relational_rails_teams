10f33f797f3335d317f6cbeebaadde198cae204c
TJ Williams <williamsjr.timothy1@gmail.com>
2022-02-09 18:11:21 -0500

    Add: seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+BasketballTeam.destroy_all
+BasketballPlayer.destroy_all
+
+@team = BasketballTeam.create!(name: "Atlanta Hawks", winning_record: true, titles: 1)
+@team2 = BasketballTeam.create!(name: "Atlanta Dream", winning_record: true, titles: 0)
+@team3 = BasketballTeam.create!(name: "Georgia Southern Eagles", winning_record: true, titles: 0)
+@team4 = BasketballTeam.create!(name: "Georgia Tech", winning_record: false, titles: 0)
+
+@player = team.basketball_players.create!(name: "Trae Young", injured: true, jersey_number: 11)
+@player2 = team.basketball_players.create!(name: "John Collins", injured: true, jersey_number: 20)
+@player3 = team2.basketball_players.create!(name: "Odyssey Sims", injured: true, jersey_number: 0)
+@player4 = team3.basketball_players.create!(name: "Jordan Usher", injured: false, jersey_number: 4)
