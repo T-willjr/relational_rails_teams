77477cd8d6ad44639747d2aa7aefdfeed0ff0a60
Paul Leonard <paulpaulleonard@gmail.com>
2022-02-09 16:15:21 -0500

    Rewright #index method in FBTPlayers controller to pass test

--- app/controllers/football_team_players_controller.rb
+++ app/controllers/football_team_players_controller.rb
@@ -4,6 +4,8 @@ class FootballTeamPlayersController < ApplicationController
     @team = FootballTeam.find(params[:id])
     if params[:sort]
       @players = @team.alpha_order
+    elsif params[:jersey_number]
+      @players = @team.jersey_numbers_higher_than(params[:jersey_number])
     else
       @players = FootballPlayer.where(football_team_id: @team.id)
     end
